351e855df163cf8c5e94fb8c144ba801dc69a3b8
SunnyNie <sunnymingnie@gmail.com>
2021-11-29 16:43:25 -0800

    add multiple youtube apis

--- youtube_checker.py
+++ youtube_checker.py
@@ -16,24 +16,24 @@ def await_for_post(strats = None):
     interval is sleep time between polling check
     """
     if strats==None: strats = read_strat()
+    i = -1 #index for api accessing, looping through apis
     while strats['long'] == None:
         hour = datetime.datetime.now(datetime.timezone(datetime.timedelta(0))).hour
-        if hour<14: interval = 30
-        elif (hour == 15): interval=2
-        else: interval = 5
-
+        if (hour==15 or hour == 16): interval = 0.3
+        elif (hour >= 14 and hour <20): interval=0.5
+        else: interval = 1
         time.sleep(interval)
-
-        strats = check_post(strats)
+        i = i+1 if i+1<len(API) else 0
+        strats = check_post(strats, API[i])
     return strats
     
 
-def check_post(strats):
+def check_post(strats, api):
     """
     for Coin Bureau channel, checks if any new post within 1 minute included any mentions if specific key words and does not contain 'news'
     If so updates strats correctly and returns the strat. Else just returns the strat
     """
-    title = get_latest_video_title()
+    title = get_latest_video_title(api)
     if 'news' in title: return strats
 
     for strat in strats['pair']:
@@ -53,14 +53,14 @@ def str_to_list(s):
         return c.isalpha() or c==" " or c.isnumeric()
     return ''.join(filter(valid_character, s)).split()
 
-def get_latest_video_title():
+def get_latest_video_title(api):
     """
     returns video title of the latest coin-bureau video
 
     For information: https://stackoverflow.com/questions/18953499/youtube-api-to-fetch-all-videos-on-a-channel
     answer by virtualmic
     """
-    url = f'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUqK_GSMbpiV8spgD3ZGloSw&key={API}&part=snippet&maxResults=1'
+    url = f'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUqK_GSMbpiV8spgD3ZGloSw&key={api}&part=snippet&maxResults=1'
     response = requests.get(url)
     title = response.json()['items'][0]['snippet']['title'].lower()
     return str_to_list(title)
