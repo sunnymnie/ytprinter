208508a20e2cab63d7f14fb81cce8d4348a5d775
SunnyNie <sunnymingnie@gmail.com>
2021-12-06 10:20:10 -0800

    minor updates

--- test.py
+++ test.py
@@ -3,8 +3,15 @@ import requests
 import json
 import keys
 
-API = keys.key('youtube', 'api')
-api = API[3]
-url = f'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUqK_GSMbpiV8spgD3ZGloSw&key={api}&part=snippet&maxResults=1'
-response = requests.get(url)
-print(response.json())
+def main():
+    API = keys.key('youtube', 'api')
+    for i in range(len(API)):
+        api = API[i]
+        url = f'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUqK_GSMbpiV8spgD3ZGloSw&key={api}&part=snippet&maxResults=1'
+        response = requests.get(url)
+        print(f"======================================[{i}]=========================================")
+        print(response.json())
+
+
+if __name__ == "__main__":
+    main()
