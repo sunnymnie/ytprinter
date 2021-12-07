7a845501ddaf3860a809e23c6fb7c5b053739bc4
SunnyNie <sunnymingnie@gmail.com>
2021-12-07 09:28:19 -0800

    tests

--- test.py
+++ test.py
@@ -4,6 +4,20 @@ import json
 import keys
 import strategy
 import os
+import random
+
+def TEST_get_latest_video_title(api):
+    print("WARNING: RUNNING TEST FUNCTION TEST_get_latest_video_title")
+    num = random.randint(0, 3)
+    if (num==0):
+        return ['omegas', 'smega', 'deltex', 'betrix', 'asdfe']
+    elif (num==1):
+        return ['asdfew', 'vrsdafs', 'dfweft', 'dsafww3', 'g3q4fd', 'dfewddd', 'g4qddd', 'dfa32e']
+    elif (num==2):
+        return []
+    else:
+        return ['33qre', '33qre', 'dfsaewfe', 'veqggrd', 'dfwq23f', 'fda3w2e', 'dfdfd2']
+            
 
 def main():
     strats = strategy.read_strat()
@@ -22,3 +36,4 @@ def main1():
 
 if __name__ == "__main__":
     main()
+else: print("WARNING: imported TEST module test.py")
