1484b32b4ead723087f2ca441ff1927fcec73e5e
SunnyNie <sunnymingnie@gmail.com>
2021-12-14 21:39:00 -0800

    revised test

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ import keys
 import strategy
 import os
 import random
-import admin
+# import admin
+import time
+import random
 
 def TEST_get_latest_video_title(api):
     print("WARNING: RUNNING TEST FUNCTION TEST_get_latest_video_title")
@@ -36,9 +38,10 @@ def main1():
         url = f'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUqK_GSMbpiV8spgD3ZGloSw&key={api}&part=snippet&maxResults=1'
         response = requests.get(url)
         print(f"======================================[{i}]=========================================")
-        print(response.json())
+        print(response)
+        time.sleep(random.randint(3,5))
 
 
 if __name__ == "__main__":
-    main()
+    main1()
 else: print("WARNING: imported TEST module test.py")
