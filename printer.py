f58ef2e130aecb07306b4b515a4eeedb0a912d3b
SunnyNie <sunnymingnie@gmail.com>
2022-01-29 15:51:52 -0800

    add schedule

--- printer.py
+++ printer.py
@@ -1,6 +1,9 @@
 from strategy import read_strat
 import youtube_checker as yc
 import portfolio_manager as pm
+import schedule
+import time
+
 # import trader
 
 def main():
@@ -15,14 +18,15 @@ def main():
         strats = pm.manage_position(strats)
         pm.init_previous_portfolio(strats)
         print("Finished trading...")
-    print(f"Printer finished successfully")
 
-
-
-
-
-
+def schedule_turn_on(start = "06:00"):
+    """schedules turning on"""
+    schedule.every().day.at(start).do(main)
+    print(f"Scheduled printer to start at {start}")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    schedule_turn_on()
